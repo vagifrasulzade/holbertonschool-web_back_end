55fec0bdb271fc0bdd12f56a8895a4bc4d09ff9c
unknown <vaqufresulzade@gmail.com>
2026-02-10 21:50:39 +0400

    .

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that defines a type-annotated add function."""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns the result."""
+    return a + b
