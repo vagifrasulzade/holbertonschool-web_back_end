c188dc60b4217d81a3851a6c22fafb996a375ad8
unknown <vaqufresulzade@gmail.com>
2026-02-10 22:16:35 +0400

    .

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates variables with specific types and values.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
