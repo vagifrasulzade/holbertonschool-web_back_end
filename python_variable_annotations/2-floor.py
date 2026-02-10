6386b775a271ad97fe1abacdf308a7399155fc9e
unknown <vaqufresulzade@gmail.com>
2026-02-10 21:58:17 +0400

    .

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""Module that defines a type-annotated floor function."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float as an integer."""
+    return math.floor(n)
